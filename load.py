c1646f7d4047287980122ee125eaa3175f009256
Anderson VInicius Ribeiro <andyreborn@hotmail.com.br>
2021-10-09 20:46:46 -0300

    Implementa função para inserção de registros na tabelas de relatorios

--- load.py
+++ load.py
@@ -1,5 +1,38 @@
 import pandas as pd
+from queries import *
 from io import StringIO
+
+# Carrega dados das planilhas de relatóros no banco de dados
+def load_reports(cur, conn, path):
+    filename = path.split('\\')[-1]
+    
+    #Transforma o csv em dataframe
+    relatorios = pd.read_csv(path,sep=';',
+                             parse_dates=['DATA'],
+                             encoding='unicode_escape')
+    
+    #Converte a coluna de saldo para o tipo float
+    relatorios['VL_SALDO_FINAL'] = relatorios['VL_SALDO_FINAL'].str.replace(',','.')
+    relatorios['VL_SALDO_FINAL'].astype('float64')
+    
+    #Cria um buffer na memória para fazer a importação dos arquivos para o db
+    buffer = StringIO()
+    relatorios.to_csv(buffer, header=False, index=False, sep=';')
+    buffer.seek(0)
+    
+    try:
+        #Importa os arquivos do buffer para o db
+        print('Inserindo registros do arquivo {}'.format(filename))
+        cur.copy_from(buffer, sep=';', table='relatorio_contabil',
+                      columns=(relatorio_columns))
+        
+        conn.commit()
+        print('Registros do arquivo {} foram inseridos'.format(filename))
+        
+    except(Exception) as error:
+        print('Error {}'.format(error))
+        conn.rollback()
+
 
 # Carrega dados das operadoras no banco de dados
 def insert_cadop(cur, conn, path):
@@ -27,3 +60,4 @@ def insert_cadop(cur, conn, path):
     except (Exception) as error:
         print("Error: %s" % error)
         conn.rollback()
+
